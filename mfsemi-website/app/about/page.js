993df69507ab13ea5ddc57ccafa6610d3470900f
Tom Pickard <tom@pickard.dev>
2025-03-03 20:19:07 -0700

    chore: Remove unused import statement in about and demo pages

--- mfsemi-website/app/about/page.js
+++ mfsemi-website/app/about/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import Layout from "../components/Layout";
 
 import Layout from "../components/Layout";
 
--- mfsemi-website/app/demo/page.js
+++ mfsemi-website/app/demo/page.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
-import Layout from "../components/Layout";
+//import Layout from "../components/Layout";
 import LorenzAttractor from "../components/LorenzAttractor";
 
 import Layout from "../components/Layout";
