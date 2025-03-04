0b51ff661848e5e074e9d01352c6185df5d6fb76
Tom Pickard <tom@pickard.dev>
2025-03-03 21:30:11 -0700

    feat: Enhance LorenzAttractor with random initial values and scaling factors

--- mfsemi-website/app/components/LorenzAttractor.js
+++ mfsemi-website/app/components/LorenzAttractor.js
@@ -1,19 +1,20 @@
 "use client";
 import React, { useRef, useEffect } from "react";
 import p5 from "p5";
-
+const scale = 2;
+const pscale = 1.01;
 export default function LorenzAttractor() {
   const sketchRef = useRef();
 
   useEffect(() => {
     const sketch = (p) => {
-      let x = 0.01,
-        y = 0,
-        z = 0;
-      const a = 10,
-        b = 28,
-        c = 8 / 3;
-      const dt = 0.01;
+      let x = Math.random(), // 0.01,
+        y = Math.random(),
+        z = Math.random();
+      const a = 10 * scale,
+        b = 28 * scale,
+        c = (8 / 3) * scale;
+      const dt = 0.005;
       let points = [];
 
       p.setup = () => {
@@ -30,11 +31,17 @@ export default function LorenzAttractor() {
         x += dx;
         y += dy;
         z += dz;
+        x *= pscale;
+        y *= pscale;
+        z *= pscale;
         points.push({ x, y, z });
-        if (points.length > 1000) {
+        if (points.length > 500 + 700 * (0.5 - Math.random())) {
           points.shift();
         }
-        p.rotateY(p.frameCount * 0.003);
+        //p.rotateY(p.frameCount * 0.003);
+        p.rotateY(y * 0.1);
+        p.rotateX(x * 0.1);
+        p.rotateZ(z * 0.1);
         p.stroke(255);
         p.noFill();
         p.beginShape();
